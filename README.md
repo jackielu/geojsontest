d9610c5ed262c48219c380ecb14c33b3c1277ca8
jackielu <jackielu@gmail.com>
2014-11-04 20:11:36 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+simple geoJSON
+=========
+
+this repo took leaflet-1, and  modified it to use geojson data instead
---
+++ js/script.js
@@ -0,0 +1,19 @@
+var map = L.map('map').setView([40.68025, -74.00155], 13);
+
+L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.png', {
+    maxZoom: 18
+}).addTo(map);
+
+var marker = L.marker([40.68025, -74.00155]).addTo(map);
+marker.bindPopup("Larry's house").openPopup();
+
+
+//
+function popUp (feature, layer) {
+	layer.bindPopup (feature.properties.att_1);
+}
+
+//get external geoJSON file
+var geojsonLayer = new L.GeoJSON.AJAX("./data/map.geojson",{onEachFeature:popUp});
+
+geojsonLayer.addTo(map);
